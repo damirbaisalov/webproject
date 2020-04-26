2fda33d55354a8801e5ff8d952bb6179a0d21757
damirbaisalov <baisalovdamir@gmail.com>
2020-04-26 19:35:33 +0600

    fixed urls and generic_views

--- storeback/api/urls.py
+++ storeback/api/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from api.views import views_fbv, views_cbv
+from api.views import views_fbv, views_cbv , views_generic
 
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('login/', obtain_jwt_token),
 
-    path('categories/', views_cbv.CategoryListAPIView.as_view()),
-    path('categories/<int:category_id>/products/', views_cbv.ProductsByCategoryAPIView.as_view()),
-    path('products/', views_cbv.ProductsAPIView.as_view()),
-    path('products/<int:product_id>/', views_cbv.ProductDetailAPIView.as_view())
+    path('categories/', views_generic.CategoryListAPIView.as_view()),
+    path('categories/<int:category_id>/products/', views_generic.CategoryProductsAPIView.as_view()),
+    path('products/', views_generic.ProductListAPIView.as_view()),
+    path('products/<int:product_id>/', views_generic.ProductDetailAPIView.as_view())
 ]
--- storeback/api/views/views_generic.py
+++ storeback/api/views/views_generic.py
@@ -1,27 +1,45 @@
+from rest_framework import status
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from api.models import Category,Product
+from api.serializers import CategorySerializer,ProductSerializer,CategoryWithProductSerlializer
 from rest_framework.permissions import IsAuthenticated
-
-from api.models import Category, Product
-
-from api.serializers import CategorySerializer, ProductSerializer
-
 
 class CategoryListAPIView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = (IsAuthenticated,)
 
-
-class CategoryDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+class CompanyDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
+    # permission_classes = (IsAuthenticated)
 
 class ProductListAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
+    # permission_classes = (IsAuthenticated)
 
 class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    # permission_classes = (IsAuthenticated)
+
+# class CompanyWithVacanciesAPIView(APIView):
+#     queryset = Vacancy.objects.all()
+#     serializer_class = CompanyWithVacanciesSerializer
+###############################################################################
+class CategoryProductsAPIView(APIView):
+    def get(self, request, category_id):
+        products = Product.objects.filter(category_id=category_id)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': serializer.errors},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    # permission_classes = (IsAuthenticated)
